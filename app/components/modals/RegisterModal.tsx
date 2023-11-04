66c0f782c111bf1b91dcdf8369ab9c07246d23f2
Maor Beeri <marmaor@gmail.com>
2023-11-04 16:49:10 +0200

    finish auth modal

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -37,7 +37,7 @@ const RegisterModal = () => {
       .post("/api/register", data)
       .then(() => registerModal.onClose())
       .catch((error) => {
-        toast.error("Something went wrong")
+        toast.error("Something went wrong");
       })
       .finally(() => setIsLoading(false));
   };
@@ -73,19 +73,48 @@ const RegisterModal = () => {
     </div>
   );
 
-
   const footerContent = (
     <div className="flex flex-col gap-3 mt-3">
-      <hr/>
+      <hr />
       <Button
-      outline
-      label="Continue woth google"
-      icon={FcGoogle}
-      onClick={()=>{}}
+        outline
+        label="Continue with Google"
+        icon={FcGoogle}
+        onClick={() => {}}
       />
-
+      <Button
+        outline
+        label="Continue with GitHub"
+        icon={AiFillGithub}
+        onClick={() => {}}
+      />
+      <div
+        className="
+      text-neutral-500
+      text-center
+      mt-4
+      font-light
+      "
+      >
+        <div
+          className="
+        justify-center flex flex-row items-cneter gap-2"
+        >
+          <div>Already have an account?</div>
+          <div
+            onClick={registerModal.onClose}
+            className="
+            text-neutral-800 
+            cursor-pointer 
+            hover:underline
+          "
+          >
+            Log in
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 
   return (
     <Modal

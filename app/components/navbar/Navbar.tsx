8cd527bccb0af8de64d458b1522af62329a7b27e
Maor Beeri <marmaor@gmail.com>
2023-11-14 17:08:03 +0200

    add Categoris to Navbar

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,10 +1,11 @@
 "use client";
 
+import { SafeUser } from "@/app/types";
 import Container from "../Container";
+import Categories from "./Categories";
 import Logo from "./Logo";
 import Search from "./Search";
 import UserMenu from "./UserMenu";
-import { SafeUser } from "@/app/types";
 
 interface NavbarProps {
   currentUser?: SafeUser | null;
@@ -36,6 +37,7 @@ const Navbar: React.FC<NavbarProps> = ({ currentUser }) => {
           </div>
         </Container>
       </div>
+      <Categories/>
     </div>
   );
 };
